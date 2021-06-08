e59d5f0fb17a8d03f53a17a8a778c899edf21280
neilkazimierzsheridan <65231357+neilkazimierzsheridan@users.noreply.github.com>
2021-06-08 20:05:08 +0100

    Added code to use the functions

---
+++ db_app.py
@@ -0,0 +1,19 @@
+#database app
+
+import database
+
+
+#database.add_one("Pinky", "Pinkerton", "pinky@gmail.com")
+#database.show_all()
+database.delete_one('1')
+database.show_all()
+
+'''new_customers_list = [
+				('Peachy', 'Peacherton', 'peachy@gmail.co.uk'),
+				('Tiny', 'Teenerton', 'tiny@google.com')]
+
+'''
+#database.add_many(new_customers_list)
+
+#database.email_lookup('tiny@google.com')
+#database.show_all()
